8591355d8cff1bf0a320159132a830ac90ced952
sammiesamm <nguyensamnguyen1011@gmail.com>
2025-04-19 02:55:45 +0700

    add register interface and connect API for Login/Register

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TimerPage from './pages/timer';
 import Homepage from './pages/homepage';
-
+import Register from './pages/register';
 import Operations from './pages/operations';
 import Login from './pages/login';
 import './App.css'
@@ -13,8 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/timerpage" element={<TimerPage />} />
-        <Route path='/login' element={<Login />} /> 
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/operations' element={<Operations />} />
+
         {/* You can add more routes here */}
       </Routes>
     </Router>
